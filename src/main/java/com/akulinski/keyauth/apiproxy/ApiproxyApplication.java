9a376c061463543be38dc47ce16b3e03755b7bd6
akulinski <albert.kulinski@hotmail.com>
2019-09-01 15:29:57 +0200

    Initial commit

---
+++ src/main/java/com/akulinski/keyauth/apiproxy/ApiproxyApplication.java
@@ -0,0 +1,17 @@
+package com.akulinski.keyauth.apiproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class ApiproxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiproxyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/akulinski/keyauth/apiproxy/ApiproxyApplicationTests.java
@@ -0,0 +1,8 @@
+package com.akulinski.keyauth.apiproxy;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiproxyApplicationTests {
+
+}
